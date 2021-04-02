aca3e6510693c0ed6e645bf011201dee5bba01a4
rsamhollyer <rsamhollyer@gmail.com>
2021-04-02 12:24:25 -0500

    created route for the FE to get the data from

---
+++ routers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  weatherRouter: require("./weather"),
+};
---
+++ routers/weather.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { weatherController } = require("../controllers");
+
+router.get("/", weatherController.getWeather);
+
+module.exports = router;
